6b4873291b8a5cb716045cbf394361d8fb341666
zp4rker <me@zp4rker.com>
2019-08-03 08:03:22 +1000

    updated to discore v2.1.0

--- src/main/kotlin/com/zp4rker/ktbot/KtBot.kt
+++ src/main/kotlin/com/zp4rker/ktbot/KtBot.kt
@@ -2,24 +2,20 @@ package com.zp4rker.ktbot
 
 import com.zp4rker.core.discord.command.Command
 import com.zp4rker.core.discord.command.CommandHandler
-import com.zp4rker.core.discord.util.sendRawString
-import emoji4j.EmojiUtils
 import net.dv8tion.jda.api.AccountType
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Guild
 import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.hooks.AnnotatedEventManager
-import net.dv8tion.jda.api.requests.restaction.MessageAction
 
-fun main() {
+fun main(args: Array<String>) {
 
     val handler = CommandHandler()
     handler.registerCommands(Test)
 
-    JDABuilder(AccountType.BOT).setToken("INSERT TOKEN HERE").setEventManager(AnnotatedEventManager()).addEventListeners(handler).build()
+    JDABuilder(AccountType.BOT).setToken(args[0]).setEventManager(AnnotatedEventManager()).addEventListeners(handler).build()
 
 }
 
