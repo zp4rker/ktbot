f827b239beda71c49e069a38b85b7e2d86224c61
zp4rker <me@zp4rker.com>
2020-04-08 13:46:14 +1000

    push

--- src/main/kotlin/com/zp4rker/ktbot/KtBot.kt
+++ src/main/kotlin/com/zp4rker/ktbot/KtBot.kt
@@ -1,26 +1,9 @@
 package com.zp4rker.ktbot
 
-import com.zp4rker.core.discord.command.Command
-import com.zp4rker.core.discord.command.CommandHandler
-import net.dv8tion.jda.api.AccountType
-import net.dv8tion.jda.api.EmbedBuilder
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.entities.Guild
-import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.entities.TextChannel
-import net.dv8tion.jda.api.hooks.AnnotatedEventManager
+import com.zp4rker.discore.api.Bot
 
-fun main(args: Array<String>) {
+class KtBot : Bot {
 
-    val handler = CommandHandler()
-    handler.registerCommands(Test)
 
-    JDABuilder(AccountType.BOT).setToken(args[0]).setEventManager(AnnotatedEventManager()).addEventListeners(handler).build()
 
-}
-
-object Test : Command(aliases = arrayOf("test")) {
-    override fun handle(message: Message, channel: TextChannel, guild: Guild, args: List<String>) {
-        EmbedBuilder().setImage("https://mcapi.us/server/image?ip=play.hivemc.com").build().also { channel.sendMessage(it).queue() }
-    }
 }
